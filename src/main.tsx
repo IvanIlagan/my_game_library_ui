78c01bbd35131d66594a3837cb6b9478956273c5
Genesis Ivan Ilagan <ivanilagan1109@gmail.com>
2024-12-27 00:38:59 +0800

    Implement Initial Pages Routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
-import App from './App.tsx'
+import GameList from "./GameList";
+import GameDetail from "./GameDetail";
 
 createRoot(document.getElementById('root')!).render(
 
   <BrowserRouter>
       <StrictMode>
-          <App />
+          <Routes>
+              <Route path="/" element={<GameList />} />
+
+              <Route path=":game" element={<GameDetail />} />
+          </Routes>
       </StrictMode>
   </BrowserRouter>
 )
