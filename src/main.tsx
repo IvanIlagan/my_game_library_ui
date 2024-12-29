ad359350cf214e200d35585aff6819a779cab3e5
Genesis Ivan Ilagan <ivanilagan1109@gmail.com>
2024-12-30 00:58:51 +0800

    feat: Create Login Page

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 import GameList from "./GameList";
 import GameDetail from "./GameDetail";
 import SignUp from "./SignUp";
+import Login from "./Login";
 
 createRoot(document.getElementById('root')!).render(
 
@@ -16,6 +17,8 @@ createRoot(document.getElementById('root')!).render(
               <Route path=":game" element={<GameDetail />} />
 
               <Route path="/sign_up" element={<SignUp />} />
+
+              <Route path="/login" element={<Login />} />
           </Routes>
       </StrictMode>
   </BrowserRouter>
