a5939f15b16b599b661047d85a749e8f45f37fe3
Genesis Ivan Ilagan <ivanilagan1109@gmail.com>
2025-01-04 16:01:21 +0800

    chore: Create Shared Styles For Modals

--- src/pages/GameDetail/RemoveGameButton.tsx
+++ src/pages/GameDetail/RemoveGameButton.tsx
@@ -26,9 +26,9 @@ export function RemoveGameButton({ gameId, gameName }) {
             </Dialog.Trigger>
 
             <Dialog.Portal>
-                <Dialog.Overlay className="fixed inset-0 bg-black/80" />
-                <Dialog.Content className="fixed w-[500px] top-1/2 left-1/2 bg-white rounded-[6px] -translate-x-1/2 -translate-y-1/2">
-                    <Dialog.Title className="m-0 font-medium text-lg p-6">
+                <Dialog.Overlay className="modal-overlay" />
+                <Dialog.Content className="modal-container">
+                    <Dialog.Title className="modal-title">
                         <div className="flex justify-between">
                             Remove Game
 
