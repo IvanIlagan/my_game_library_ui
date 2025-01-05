d2b74abecada05dd56df29e85d43ec71bca22a84
Genesis Ivan Ilagan <ivanilagan1109@gmail.com>
2025-01-05 17:55:24 +0800

    enhance: Add Styles to Filters Bar in My Games List

--- src/pages/GameList/GameList.tsx
+++ src/pages/GameList/GameList.tsx
@@ -58,20 +58,35 @@ function GameList() {
 
     const gamesList = games.map((item) => {
         return (
-        <button className="" key={item.gb_game_id} onClick={() => onGameClick(item)}>
-            <img src={item.image_url} className="w-full h-[92%] object-fill" />
-            <div
-                className="flex justify-center items-center w-full h-[8%] font-bold text-center bg-[#E1E1E1] p-auto rounded-b-lg overflow-hidden">
-                <span className="truncate">{item.name}</span>
-            </div>
-        </button>
-    )
+            <button className="" key={item.gb_game_id} onClick={() => onGameClick(item)}>
+                <img src={item.image_url} className="w-full h-[92%] object-fill"/>
+                <div
+                    className="flex justify-center items-center w-full h-[8%] font-bold text-center bg-[#E1E1E1] p-auto rounded-b-lg overflow-hidden">
+                    <span className="truncate">{item.name}</span>
+                </div>
+            </button>
+        )
     });
 
     return (
         <>
-            <div className="navbar flex justify-between items-center mb-6">
+            <div className="flex justify-between items-center">
                 <h1 className="text-6xl bold">{name}'s Games</h1>
+            </div>
+
+            <div className="list-filter flex justify-end items-center px-4 mb-6 bg-blue-200">
+                <div className="h-[50px] mr-4">
+                    <button className="form-button h-full w-[110px] border-l border-black/30 rounded-none text-black">
+                        <div className="flex items-center justify-center">
+                            Name
+                        </div>
+                    </button>
+                    <button className="form-button h-full w-[130px] border-x border-black/30 rounded-none text-black">
+                        <div className="flex items-center justify-center">
+                            Added Date
+                        </div>
+                    </button>
+                </div>
                 <div className="flex justify-center items-center">
                     <input type="text"
                            className="form-input mt-0 mr-4"
@@ -82,11 +97,8 @@ function GameList() {
                 </div>
             </div>
 
-            <div className="list-filter mb-6">
-                filters here
-            </div>
-
-            <div className="games-list grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-[0_5%]">
+            <div
+                className="games-list grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-[0_5%]">
                 {gamesList}
             </div>
         </>
